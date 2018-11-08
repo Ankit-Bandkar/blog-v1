f24a6620a9035eb8879b5bb9d4aa8c11c108b826
ItsaDopeShow <ankitbandkarr@gmail.com>
2018-11-08 17:04:59 +0530

    adding css,styling messages and errors

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -12,11 +12,11 @@ class ArticlesController < ApplicationController
     def create
         @article = Article.new(article_params)
         if @article.save
-            flash[:notice] = "Artice was successfully created."
+            flash[:success] = "Artice was successfully created."
             redirect_to article_path(@article)
         else
             render :new
-        end
+        end 
     end
 
     def show
@@ -27,14 +27,14 @@ class ArticlesController < ApplicationController
 
     def destroy
         @article.destroy
-        flash[:notice] = "Article was succesfully deleted."
+        flash[:danger] = "Article was succesfully deleted."
         redirect_to articles_path
     end
 
 
     def update
         if @article.update(article_params)
-            flash[:notice] = "Article was successfully edited"
+            flash[:success] = "Article was successfully edited"
             redirect_to article_path(@article)
         else
             render :edit
