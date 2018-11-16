8639b8da829ee92263d4355425c3bc959c86a21b
ItsaDopeShow <ankitbandkarr@gmail.com>
2018-11-16 19:02:50 +0530

    basic functionality completed

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,6 +5,7 @@ class ArticlesController < ApplicationController
 
     def index
         @articles = Article.paginate(page: params[:page], per_page: 4)
+        @categories = Category.all
     end
 
     def new
